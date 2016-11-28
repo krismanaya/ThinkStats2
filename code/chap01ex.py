0ce547afbf7ad7e31d3b66946373070b848a4c5d
krismanaya <krismanaya@gmail.com>
2016-11-28 14:58:55 -0800

    pass some tests

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -42,12 +42,43 @@ def MakePregMap(df):
 def IntersectPregRespMap(): 
 	"""Creates a set that maps from each caseid to a list of indices 
 
-	into the pregnancy DataFrame."""
+	into the pregnancy DataFrame. This is what I believe the question to be
+	all the cases from the female respondents and pregnum account. I 
+	bellieve this to be a better version."""
 	
 	keys_a = set(MakePregMap(nsfg.ReadFemPreg()).keys())
 	keys_b = set(MakePregMap(ReadFemResp()).keys())
 	intersection = keys_a & keys_b
 	return intersection
+
+def ValidatePregnum(df): 
+	"""Validate pregnum in the respondent file.
+
+	df: respondent DataFrame, this code is copied
+	from the solution because 
+	I didn't understand the question. 
+
+	"""
+
+	#read the pregnancy frame 
+	preg = nsfg.ReadFemPreg()
+
+	#make the map from caseid to list of pregnancy indices 
+	preg_map = nsfg.MakePregMap(preg)
+
+	#iterate through the respondent pregnum series 
+	for index, pregnum in df.pregnum.iteritems(): 
+		caseid = df.caseid[index] #dictionary 
+		indices = preg_map[caseid] #dictionary 
+
+		# check that pregnum from the respondent file equals 
+		# the number of record in the pregnancy file 
+		if len(indices) != pregnum: 
+			print(caseid, len(indices), pregnum)
+			return False 
+
+	return True 
+
 
 
 def main(script):
@@ -60,7 +91,7 @@ def main(script):
     # print(nsfg.MakePregMap(df))
 
     assert len(df) == 7643 
-    
+    assert (ValidatePregnum(df))
     assert df.pregnum.value_counts()[0] == 2610
     assert df.pregnum.value_counts()[1] == 1267
     assert df.pregnum.value_counts()[2] == 1432 
